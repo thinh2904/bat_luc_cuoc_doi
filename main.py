7904c1dfef3147140fb333fbecad96259850dca9
thinh2904 <nguyentienthinh13a4hbt@gmail.com>
2021-02-03 14:02:55 +0700

    a

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import find_url
 import save_url
 
 
-start_url = input('Cho xin cái địa chỉ: ')
-max = int(input('Số trang: '))
+start_url = input('Người anh em cho xin cái địa chỉ: ')
+max = int(input('Số trang cần tìm: '))
 url_list = find_url.find_url(start_url, start_url)
 history = find_url.find_next_url(url_list, start_url, max)
-save_url.new_folder(input('Folder name: '))
+save_url.new_folder(input('Tên thư mục lưu: '))
 save_url.save_all_file(history, max)
